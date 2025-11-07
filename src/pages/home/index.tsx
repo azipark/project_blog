6bcae2da0a2593ca0d6586a5d960a5bbb2f2b006
hy7721 <azipark7721@gmail.com>
2025-11-07 12:26:12 +0900

    홈 페이지 게시글 리스트 UI 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,11 +10,35 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post__list">post list</div>
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체</div>
+        <div>나의 글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile"></div>
+                <div className="post__author-name">hy</div>
+                <div className="post__date">2025.11.01</div>
+              </div>
+              <div className="post__title">게시글 {index}</div>
+              <div className="post__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum natus harum at voluptate tempora a optio perferendis ab nostrum minima aut beatae aliquam, voluptates itaque excepturi necessitatibus voluptas odit voluptatibus!
+              </div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>Menu 1</div>
-        <div>Menu 2</div>
-        <div>Menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   );
